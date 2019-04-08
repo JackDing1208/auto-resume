8d8192b6978bb75be49effb1df48efee1f1b195a
JackDing <361164217@qq.com>
2019-04-08 21:30:17 +0800

    功能完成

--- main.js
+++ main.js
@@ -1,37 +1,35 @@
-
-
-
 var preCode = ""
+var speed=50
 
 
-var printCode = function (code,fn) {
+var printCode = function (code, fn) {
     let n = 0
     let IDE = document.getElementById('code')
     let style = document.getElementById('style')
     let timer = setInterval(function () {
         n += 1
         IDE.innerHTML = Prism.highlight(preCode + code.substring(0, n), Prism.languages.css, 'css')
-        style.innerHTML =preCode+ code.substring(0, n)
+        style.innerHTML = preCode + code.substring(0, n)
         IDE.scrollTop = IDE.scrollHeight   //每次滚动到最下面
         if (n > code.length) {
             window.clearInterval(timer)
-            preCode+=code
+            preCode += code
             fn && fn.call()    //回调的函数,判断fn存在则执行后面代码
         }
-    }, 10)
+    }, speed)
 }
-var createPaper=function (fn) {
-    let main=document.querySelector('main')
-    let paper=document.createElement('pre')
-    paper.id="paper"
-    paper.className="paper"
-    main.appendChild(paper)
+var createPaper = function (fn) {
+    let wrapper = document.querySelector('.paper-wrapper')
+    let paper = document.createElement('pre')
+    paper.id = "paper"
+    paper.className = "paper"
+    wrapper.appendChild(paper)
     fn && fn.call()
 }
 
 
-var writeResume=function (content,fn) {
-    let paper=document.getElementById('paper')
+var writeResume = function (content, fn) {
+    let paper = document.getElementById('paper')
     let n = 0
     let timer = setInterval(function () {
         n += 1
@@ -41,16 +39,20 @@ var writeResume=function (content,fn) {
             window.clearInterval(timer)
             fn && fn.call()
         }
-    }, 10)
+    }, speed)
 }
 
-var convertMarkdown=function (md,fn) {
-    let paper=document.getElementById('paper')
-    paper.innerHTML = marked(md)
+var convertMarkdown = function (md, fn) {
+    let paper = document.getElementById('paper')
+    let wrapper = document.querySelector('.paper-wrapper')
+    paper.innerHTML = ''        //pre元素会影响markdown格式
+    let markdown = document.createElement('div')
+    markdown.id="markdown"
+    markdown.classList.add('markdown-body')
+    markdown.innerHTML=marked(md)
+    wrapper.appendChild(markdown)
     fn && fn.call()
 }
-
-
 
 
 var html1 = `/*
@@ -90,27 +92,30 @@ html{
 */
 
 `
-var html2=`
-#paper{   
+var html2 = `
+.paper-wrapper{   
     height:100%;
     width:50%;
     background:white;
     border:16px solid bisque;
-    padding:20px;
-    box-shadow: 0 0 10px rgba(0,0,0,0.2)
 }
 
 /* 
 *接下来可以在纸上写简历了
 */
 `
-var html3=`
+var html3 = `
 /* 
 *简历内容写的差不多了
 *接下来转成marked.js转成HTML形式
 */
 `
-
+var html4 = `
+/* 
+*目测功能都写完了
+*以后再来加样式吧
+*/
+`
 
 var md = `
 # 自我介绍
@@ -119,28 +124,34 @@ var md = `
 XXX 学校毕业
 自学前端半年
 希望应聘前端开发岗位
+
 # 技能介绍
 熟悉 JavaScript CSS
 # 项目介绍
 1. XXX 轮播
 2. XXX 简历
 3. XXX 画板
+
 # 联系方式
 - QQ xxxxxxxx
 - Email xxxxxxxx
 - 手机 xxxxxxx
+
 # 联系方式
 - QQ xxxxxxxx
 - Email xxxxxxxx
 - 手机 xxxxxxx
+
 # 联系方式
 - QQ xxxxxxxx
 - Email xxxxxxxx
 - 手机 xxxxxxx
+
 # 联系方式
 - QQ xxxxxxxx
 - Email xxxxxxxx
 - 手机 xxxxxxx
+
 # 联系方式
 - QQ xxxxxxxx
 - Email xxxxxxxx
@@ -148,13 +159,14 @@ XXX 学校毕业
 `
 
 
-
-printCode(html1,()=>{
-    createPaper(()=>{
-        printCode(html2,()=>{
-            writeResume(md,()=>{
-                printCode(html3,()=>{
-                    convertMarkdown(md)
+printCode(html1, () => {
+    createPaper(() => {
+        printCode(html2, () => {
+            writeResume(md, () => {
+                printCode(html3, () => {
+                    convertMarkdown(md,()=>{
+                        printCode(html4)
+                    })
                 })
             })
         })
